b8b227c8f2e8522ce127277ff850f6cf2cf6c131
lewiseman <lewisemann@gmail.com>
2025-01-09 15:23:34 +0300

    web redirect

--- lib/modules/user/services/user_srv.dart
+++ lib/modules/user/services/user_srv.dart
@@ -243,7 +243,7 @@ class UserServiceNotifier extends StateNotifier<models.User?> {
   Future<void> googleSignIn() async {
     await appwriteAccount.createOAuth2Session(
       provider: OAuthProvider.google,
-      success: 'https://www.papps.io/auth.html',
+      success: 'https://www.papps.io/auth_redirect.html',
       failure: '',
     );
     await Future<void>.delayed(const Duration(seconds: 1));
